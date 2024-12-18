0af4e54e8f5b621b786dfac1854ef2d192e75e22
Dhearf <119830781+Dhearf@users.noreply.github.com>
2024-12-18 20:18:27 +0700

    Client.php

--- finalprojek/rest/Client.php
+++ finalprojek/rest/Client.php
@@ -7,11 +7,11 @@ $url = "http://localhost/finalprojek/rest/Server.php";
 // HTTP GET request ke server
 $response = file_get_contents($url);
 
-// Periksa apakah ada respon dari server
+
 if ($response !== false) {
     $data = json_decode($response, true);
 
-    // Periksa apakah respon valid dan merupakan array
+    
     if (is_array($data) && !isset($data['message'])) {
         // Iterasi untuk menampilkan semua data wisata
         foreach ($data as $wisata) {
@@ -23,7 +23,7 @@ if ($response !== false) {
             echo "Kontak: " . $wisata['Kontak_Lokasi'] . "\n\n";
         }
     } elseif (isset($data['message'])) {
-        // Jika ada pesan dari server
+
         echo $data['message'] . "\n";
     } else {
         echo "Terjadi kesalahan saat memproses data dari server.\n";
